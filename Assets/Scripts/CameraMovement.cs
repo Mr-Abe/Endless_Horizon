97f441aef65e01b639f29a55c4e6d83b854b3715
mrabe <119377891+Mr-Abe@users.noreply.github.com>
2023-10-31 18:52:53 -0400

    refining parallax

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -2,18 +2,14 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target; // Reference to the player
-    public float followSpeed = 5f; // Speed at which the camera follows the player
-    public Vector3 offset = new Vector3(0, 0, -10); // Keeps the camera slightly behind the player
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
 
-    private void LateUpdate()
+    void FixedUpdate()
     {
-        // Desired position is where the camera wants to be
-        Vector3 desiredPosition = new Vector3(target.position.x, target.position.y, offset.z);
-        
-        // Lerp smoothly interpolates between the camera's current position and its desired position
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, followSpeed * Time.deltaTime);
-        
-        transform.position = smoothedPosition;
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = new Vector3(smoothedPosition.x, transform.position.y, transform.position.z);
     }
 }
